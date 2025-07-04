3a6f505e149283b803dfa69a95b9f912220c876d
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2025-07-04 10:04:33 +0200

    fix a bug

--- src/main/java/com/nexa/nexafight/service/FightService.java
+++ src/main/java/com/nexa/nexafight/service/FightService.java
@@ -23,8 +23,29 @@ public class FightService {
     }
 
     public FighterDTO fight(final int idFighter1, final int idFighter2) {
-        FighterDTO fighter1 = FighterRepository.toDTO(this.fighterRepository.findById(idFighter1));
-        FighterDTO fighter2 = FighterRepository.toDTO(this.fighterRepository.findById(idFighter2));
+        boolean isFighter1Valid = this.checkFighter(idFighter1);
+        boolean isFighter2Valid = this.checkFighter(idFighter2);
+
+        if (!isFighter1Valid && !isFighter2Valid) {
+            return new FighterDTO(0, "Invalid fighter ID(s)", 0, 0, 0, 0, "", "draw");
+        }
+
+        FighterDTO winner = null;
+        FighterDTO fighter1 = null;
+        FighterDTO fighter2 = null;
+
+        if (isFighter1Valid) fighter1 = FighterRepository.toDTO(this.fighterRepository.findById(idFighter1));
+        if (isFighter2Valid) fighter2 = FighterRepository.toDTO(this.fighterRepository.findById(idFighter2));
+
+        if (!isFighter1Valid || !isFighter2Valid) {
+            if (isFighter1Valid) {
+                winner = fighter1;
+            } else {
+                winner = fighter2;
+            }
+            this.fightRepository.addFight(idFighter1, idFighter2, winner != null ? winner.id() : 0);
+            return winner;
+        }
 
         int pvFighter1 = Objects.requireNonNull(fighter1).constitution();
         int pvFighter2 = Objects.requireNonNull(fighter2).constitution();
@@ -33,7 +54,6 @@ public class FightService {
             pvFighter1 -= FightService.attack(fighter2, fighter1) ? FightService.damage(fighter2) : 0;
             pvFighter2 -= FightService.attack(fighter1, fighter2) ? FightService.damage(fighter1) : 0;
         }
-        FighterDTO winner = null;
         if (pvFighter1 <= 0) winner = fighter2;
         if (pvFighter2 <= 0) winner = fighter1;
         if (pvFighter1 <= 0 && pvFighter2 <= 0) winner = null;
@@ -56,6 +76,13 @@ public class FightService {
         restTemplate.postForObject(fighter1.url() + "/fight",
                                    null, Void.class);
         return winner != null ? winner : new FighterDTO(0, "draw", 0, 0, 0, 0, "", "draw");
+    }
+
+    private boolean checkFighter(final int idFighter) {
+        RestTemplate restTemplate = new RestTemplate();
+        String answer = restTemplate.getForObject("http://localhost:8085/check-fighter?id=" + idFighter + "&verbose=n",
+                                                  String.class);
+        return "true".equalsIgnoreCase(answer);
     }
 
     private static boolean attack(final FighterDTO attacker, final FighterDTO defender) {
